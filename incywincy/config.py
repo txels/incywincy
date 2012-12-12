2ec9ad0416de8d44ea327796d2d25992a7dae675
Carles Barrobés <carles@barrobes.com>
2012-12-12 23:18:19 +0000

    Simplify the initialisation of settings

--- incywincy/config.py
+++ incywincy/config.py
@@ -1,15 +1,12 @@
-loading = False
-
-
 class Settings(dict):
     """
-    Lazyly loaded settings.
+    Application settings, that will typically be loaded from a module.
     """
-    def __init__(self, module):
-        self.module = module
+    def __init__(self):
         self._mod = None
 
-    def load(self):
+    def load(self, module):
+        self.module = module
         if self._mod is None:
             self._mod = __import__(self.module, globals(), locals(), [], -1)
             for attr in dir(self._mod):
@@ -17,12 +14,10 @@ class Settings(dict):
                     self[attr] = getattr(self._mod, attr)
 
     def __getattr__(self, attr):
-        global loading
-        if not loading:
-            loading = True
-            self.load()
-            loading = False
         return self[attr]
 
     def __setattr__(self, attr, value):
         self[attr] = value
+
+
+settings = Settings()
--- incywincy/spider.py
+++ incywincy/spider.py
@@ -11,12 +11,13 @@ Options:
 """
 if __name__ == '__main__':
     from docopt import docopt
-    from incywincy import __version__, config, report
+    from incywincy import __version__, report
+    from incywincy.config import settings
+    from incywincy.http import visit
 
     arguments = docopt(__doc__, version='Incy Wincy ' + __version__)
-    print arguments
-    config.settings = config.Settings(arguments['<settings>'])
-    settings = config.settings
+    # print arguments
+    settings.load(arguments['<settings>'])
     report.THRESHOLD = int(arguments['--level'])
 
     settings.root = arguments['<url>']
@@ -27,5 +28,4 @@ if __name__ == '__main__':
     if settings.start is None:
         settings.start = settings.root
 
-    from incywincy.http import visit
     visit(settings.start, settings.root)
